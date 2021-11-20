37f53da6f15a9fa4362d5b405d26fe341b6fc653
VA-Pe4enka <va.pe4enka@gmail.com>
2021-11-20 21:20:44 +0300

    refactoring

--- play.go
+++ play.go
@@ -43,95 +43,7 @@ func CreateItems() {
 var BossHealth int
 var BossDamage int
 var BossSpeed int
-var BossNames []string
 
-var chooseBoss int
-var ChooseBoss []float64
-func CreateBosses() {
-
-	bee := characters.GiantBee{}
-	bee.SetHealth()
-	bee.SetDamage()
-	bee.SetSpeed()
-
-	rabbit := characters.CrazyRabbit{}
-	rabbit.SetHealth()
-	rabbit.SetDamage()
-	rabbit.SetSpeed()
-
-	bear := characters.BearRod{}
-	bear.SetHealth()
-	bear.SetDamage()
-	bear.SetSpeed()
-
-
-	ChooseBoss = append(ChooseBoss, 1)
-	ChooseBoss = append(ChooseBoss, 2)
-	ChooseBoss = append(ChooseBoss, 3)
-
-	BossNames = append(BossNames, "Королева пчел")
-	BossNames = append(BossNames, "Бешеный кролик")
-	BossNames = append(BossNames, "Медведь шатун")
-
-BossPick:
-	fmt.Println("Выбирете противника:")
-	fmt.Println("1. ", BossNames[0])
-	fmt.Println("2. ", BossNames[1])
-	fmt.Println("3. ", BossNames[2])
-
-	reader := bufio.NewReader(os.Stdin)
-	choiceStr, _ := reader.ReadString('\n')
-	choiceStr = strings.TrimSpace(choiceStr)
-	choice, _ := strconv.ParseFloat(choiceStr, 32)
-	chooseBoss = int(choice)
-
-	switch choice {
-	case 1:
-		if strings.Contains(BossNames[chooseBoss], "использовано") {
-			fmt.Println("Вы уже использовали расходник!")
-			goto BossPick
-		} else {
-		fmt.Println("Выбран противник - Королева пчел!")
-		BossHealth = bee.BossHealth
-		fmt.Println("Здоровье противника: ", BossHealth)
-		BossDamage = bee.BossDamage
-		fmt.Println("Урон противника: ", BossDamage)
-		BossSpeed = bee.BossSpeed
-		fmt.Println("Скорость противника: ", BossSpeed)
-		}
-	case 2:
-		if strings.Contains(BossNames[chooseBoss], "использовано") {
-			fmt.Println("Вы уже использовали расходник!")
-			goto BossPick
-		} else {
-			fmt.Println("Выбран противник - Бешеный кролик!")
-			BossHealth = rabbit.BossHealth
-			fmt.Println("Здоровье противника: ", BossHealth)
-			BossDamage = rabbit.BossDamage
-			fmt.Println("Урон противника: ", BossDamage)
-			BossSpeed = rabbit.BossSpeed
-			fmt.Println("Скорость противника:", BossSpeed)
-		}
-	case 3:
-		if strings.Contains(BossNames[chooseBoss], "использовано") {
-			fmt.Println("Вы уже использовали расходник!")
-			goto BossPick
-		} else {
-			fmt.Println("Выбран противник - Медведь шатун!")
-			BossHealth = bear.BossHealth
-			fmt.Println("Здоровье противника: ", BossHealth)
-			BossDamage = bear.BossDamage
-			fmt.Println("Урон противника: ", BossHealth)
-			BossSpeed = bear.BossSpeed
-			fmt.Println("Скорость противника: ", BossSpeed)
-		}
-	default:
-		fmt.Println("Такого противника пока нет :)")
-		goto BossPick
-
-	}
-
-}
 
 var Health int
 var Armored int
@@ -139,7 +51,7 @@ var Damage int
 var Speed int
 
 func ChooseHero() {
-
+pers:
 	fmt.Println("Выберете персонажа:")
 	fmt.Println("1. Разведчик")
 	fmt.Println("2. Охотник")
@@ -203,6 +115,7 @@ func ChooseHero() {
 
 	default:
 		fmt.Println("Такого персонажа пока нет :)")
+		goto pers
 	}
 
 }
@@ -229,6 +142,60 @@ func SpeedUp() {
 
 func Fight() {
 
+	bee := characters.GiantBee{}
+	bee.SetHealth()
+	bee.SetDamage()
+	bee.SetSpeed()
+
+	fmt.Println("Первый противник - Королева пчел!")
+	BossHealth = bee.BossHealth
+	fmt.Println("Здоровье противника: ", BossHealth)
+	BossDamage = bee.BossDamage
+	fmt.Println("Урон противника: ", BossDamage)
+	BossSpeed = bee.BossSpeed
+	fmt.Println("Скорость противника: ", BossSpeed)
+
+	Battle()
+
+	fmt.Println("Поебда... Впереди еще есть враги...")
+
+	rabbit := characters.CrazyRabbit{}
+	rabbit.SetHealth()
+	rabbit.SetDamage()
+	rabbit.SetSpeed()
+
+	fmt.Println("Второй противник - Бешеный кролик!")
+	BossHealth = rabbit.BossHealth
+	fmt.Println("Здоровье противника: ", BossHealth)
+	BossDamage = rabbit.BossDamage
+	fmt.Println("Урон противника: ", BossDamage)
+	BossSpeed = rabbit.BossSpeed
+	fmt.Println("Скорость противника:", BossSpeed)
+
+	Battle()
+
+	fmt.Println("Очередная битва позади... Остался послсдений противник!")
+
+	bear := characters.BearRod{}
+	bear.SetHealth()
+	bear.SetDamage()
+	bear.SetSpeed()
+
+	fmt.Println("Выбран противник - Медведь шатун!")
+	BossHealth = bear.BossHealth
+	fmt.Println("Здоровье противника: ", BossHealth)
+	BossDamage = bear.BossDamage
+	fmt.Println("Урон противника: ", BossHealth)
+	BossSpeed = bear.BossSpeed
+	fmt.Println("Скорость противника: ", BossSpeed)
+
+	Battle()
+
+	fmt.Println("ПОБЕДА!")
+}
+
+func Battle(){
+
 	fmt.Println("Битва начинается!!!")
 	fmt.Println("Выбирете действие:")
 
@@ -237,7 +204,7 @@ func Fight() {
 	Action = append(Action, "Использовать расходник")
 	Action = append(Action, "Отступить")
 
-	Act:
+Act:
 	fmt.Println("1. ", Action[0])
 	fmt.Println("2. ", Action[1])
 	fmt.Println("3. ", Action[2])
@@ -273,11 +240,9 @@ func Fight() {
 
 		if Health < 1{
 			fmt.Println("Вы погибли!")
+			os.Exit(0)
 		} else if BossHealth < 1 {
 			fmt.Println("Босс повержен!")
-			BossNames[chooseBoss] = BossNames[chooseBoss] + "(побежден)"
-
-
 		} else {
 			goto Act
 		}
@@ -334,13 +299,12 @@ func Fight() {
 
 }
 
+
 func main() {
 
 	CreateItems()
 
 	ChooseHero()
-
-	CreateBosses()
 
 	Fight()
 }
